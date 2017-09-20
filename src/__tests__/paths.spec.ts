8866935b5b0d5e61c69a0e631ce5aca52d7e6fdc
Muhammed Thanish <mnmtanish@gmail.com>
2017-09-20 13:47:24 +0530

    Add makep utility function

--- src/__tests__/paths.spec.ts
+++ src/__tests__/paths.spec.ts
@@ -1,4 +1,4 @@
-import { split, check } from '../paths';
+import { split, check, makep } from '../paths';
 
 describe( 'split', () => {
     const cases: any[] = [
@@ -49,75 +49,112 @@ describe( 'split', () => {
 
 describe( 'check', () => {
     const cases: any[] = [
-        // empty segments
-        { out: true, doc: undefined, segments: [] },
-        { out: true, doc: null, segments: [] },
-        { out: true, doc: 0, segments: [] },
-        { out: true, doc: 1, segments: [] },
-        { out: true, doc: {}, segments: [] },
-        { out: true, doc: [], segments: [] },
-        { out: true, doc: '', segments: [] },
+        // empty path
+        { out: true, doc: undefined, path: [] },
+        { out: true, doc: null, path: [] },
+        { out: true, doc: 0, path: [] },
+        { out: true, doc: 1, path: [] },
+        { out: true, doc: {}, path: [] },
+        { out: true, doc: [], path: [] },
+        { out: true, doc: '', path: [] },
 
         // empty document (object)
-        { out: true, doc: {}, segments: [] },
-        { out: true, doc: {}, segments: ['a'] },
-        { out: true, doc: {}, segments: ['a', 'b'] },
-        { out: true, doc: {}, segments: ['a', 1] },
-        { out: false, doc: {}, segments: [1] },
-        { out: false, doc: {}, segments: [1, 'a'] },
+        { out: true, doc: {}, path: [] },
+        { out: true, doc: {}, path: ['a'] },
+        { out: true, doc: {}, path: ['a', 'b'] },
+        { out: true, doc: {}, path: ['a', 1] },
+        { out: false, doc: {}, path: [1] },
+        { out: false, doc: {}, path: [1, 'a'] },
 
         // empty document (array)
-        { out: true, doc: [], segments: [] },
-        { out: true, doc: [], segments: [1] },
-        { out: true, doc: [], segments: [1, 2] },
-        { out: true, doc: [], segments: [1, 'a'] },
-        { out: false, doc: [], segments: ['a'] },
-        { out: false, doc: [], segments: ['a', 1] },
+        { out: true, doc: [], path: [] },
+        { out: true, doc: [], path: [1] },
+        { out: true, doc: [], path: [1, 2] },
+        { out: true, doc: [], path: [1, 'a'] },
+        { out: false, doc: [], path: ['a'] },
+        { out: false, doc: [], path: ['a', 1] },
 
         // documents starting with an object
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: [] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['a'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['b'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['c'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['e'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['x'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['c', 'd'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['c', 'x'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['c', 'x', 'y'] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['e', 0] },
-        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['e', 99] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['a', 'x'] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['a', 1] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['b', 'x'] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['b', 1] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['c', 1] },
-        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, segments: ['e', 'x'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: [] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['a'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['b'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['c'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['e'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['x'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['c', 'd'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['c', 'x'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['c', 'x', 'y'] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['e', 0] },
+        { out: true, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['e', 99] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['a', 'x'] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['a', 1] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['b', 'x'] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['b', 1] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['c', 1] },
+        { out: false, doc: { a: 1, b: 'b', c: { d: 'd' }, e: [ 'e' ] }, path: ['e', 'x'] },
 
         // documents starting with an array
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [0] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [1] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [99] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [0, 'a'] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [0, 'b'] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [0, 'x'] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [1, 0] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [1, 1] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [1, 99] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [99, 'x'] },
-        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], segments: [99, 99] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [0] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [1] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [99] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [0, 'a'] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [0, 'b'] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [0, 'x'] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [1, 0] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [1, 1] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [1, 99] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [99, 'x'] },
+        { out: true, doc: [{ a: 1, b: 'b' }, [2, 'c']], path: [99, 99] },
 
         // invalid parameters
-        { out: false, doc: {}, segments: undefined },
-        { out: false, doc: {}, segments: null },
-        { out: false, doc: {}, segments: 0 },
-        { out: false, doc: {}, segments: 1 },
-        { out: false, doc: {}, segments: [null] },
+        { out: false, doc: {}, path: undefined },
+        { out: false, doc: {}, path: null },
+        { out: false, doc: {}, path: 0 },
+        { out: false, doc: {}, path: 1 },
+        { out: false, doc: {}, path: [null] },
     ];
 
     cases.forEach(c => {
-        it(`should return '${JSON.stringify(c.out)}' if input is '${JSON.stringify(c.segments)}'`, () => {
-            expect(check(c.doc, c.segments)).toEqual(c.out);
+        it(`should return '${JSON.stringify(c.out)}' if input is '${JSON.stringify(c.path)}'`, () => {
+            expect(check(c.doc, c.path)).toEqual(c.out);
+        });
+    });
+});
+
+describe( 'makep', () => {
+    const cases: any[] = [
+        // empty path
+        { doc: {}, path: [], exp: {} },
+        { doc: [], path: [], exp: [] },
+
+        // empty input object
+        { doc: {}, path: ['a'], exp: {} },
+        { doc: {}, path: ['a', 'b'], exp: { a: {} } },
+        { doc: {}, path: ['a', 0], exp: { a: [] } },
+
+        // empty input array
+        { doc: [], path: [0], exp: [] },
+        { doc: [], path: [1], exp: [] },
+        { doc: [], path: [0, 1], exp: [[]] },
+        { doc: [], path: [0, 'a'], exp: [{}] },
+
+        // path to existing fields
+        { doc: { a: {}, b: [] }, path: ['a'], exp: { a: {}, b: [] } },
+        { doc: { a: {}, b: [] }, path: ['b'], exp: { a: {}, b: [] } },
+        { doc: { a: {}, b: [] }, path: ['x'], exp: { a: {}, b: [] } },
+        { doc: { a: {}, b: [] }, path: ['a', 'x'], exp: { a: {}, b: [] } },
+        { doc: { a: {}, b: [] }, path: ['b', 0], exp: { a: {}, b: [] } },
+        { doc: { a: {}, b: [] }, path: ['x', 'y'], exp: { a: {}, b: [], x: {} } },
+        { doc: { a: {}, b: [] }, path: ['x', 'y', 'z'], exp: { a: {}, b: [], x: { y: {} } } },
+        { doc: { a: {}, b: [] }, path: ['x', 0], exp: { a: {}, b: [], x: [] } },
+        { doc: { a: {}, b: [] }, path: ['x', 0, 1], exp: { a: {}, b: [], x: [[]] } },
+    ];
+
+    cases.forEach(c => {
+        it(`should change the document to '${JSON.stringify(c.exp)}' if input is '${JSON.stringify(c.doc)}'`, () => {
+            makep(c.doc, c.path);
+            expect(c.doc).toEqual(c.exp);
         });
     });
 });
--- src/paths.ts
+++ src/paths.ts
@@ -1,6 +1,10 @@
 // PathSegment
-// PathSegment ...
+// PathSegment is a path segment
 export type PathSegment = string | number;
+
+// Path
+// Path is an array of path segments
+export type Path = PathSegment[];
 
 // split
 // split breaks down a path string to segments. Each segment can be
@@ -12,45 +16,45 @@ export function split(str: string): PathSegment[] | null {
     if (typeof str !== 'string') {
         return null;
     }
-    // https://regex101.com/r/Za3mjz/1
+    // regex examples: https://regex101.com/r/Za3mjz/2
     const SPLIT_REGEX = /\.?(\w+)|\[['"]([^'"]+)['"]\]/gi;
-    const segments = [];
+    const path = [];
     while (true) {
-        const results = SPLIT_REGEX.exec(str);
-        if (!results) {
+        const matches = SPLIT_REGEX.exec(str);
+        if (!matches) {
             break;
         }
-        for (let i = 1, l = results.length; i < l; ++i) {
-            const matchStr = results[i];
+        for (let i = 1, l = matches.length; i < l; ++i) {
+            const matchStr = matches[i];
             if (matchStr) {
                 const matchNum = +matchStr;
-                const match = Number.isNaN(matchNum) ? matchStr : matchNum;
-                segments.push(match);
+                const segment = Number.isNaN(matchNum) ? matchStr : matchNum;
+                path.push(segment);
                 break;
             }
         }
     }
-    return segments;
+    return path;
 }
 
 // check
-// check ...
-export function check(doc: any, segments: PathSegment[]): boolean {
-    if (!Array.isArray(segments)) {
+// check checks whether the path can exist on the document.
+export function check(doc: any, path: Path): boolean {
+    if (!Array.isArray(path)) {
         return false;
     }
     let parent = doc;
-    for (let i = 0, l = segments.length; i<l; ++i) {
+    for (let i = 0, l = path.length; i<l; ++i) {
         if (parent === undefined) {
             break;
         }
-        const segment = segments[i];
+        const segment = path[i];
         if (typeof segment === 'string') {
-            if (typeof parent !== 'object' || parent === null || Array.isArray(parent)) {
+            if (!isObject(parent)) {
                 return false;
             }
         } else if (typeof segment === 'number') {
-            if (!Array.isArray(parent)) {
+            if (!isArray(parent)) {
                 return false;
             }
         } else {
@@ -59,4 +63,37 @@ export function check(doc: any, segments: PathSegment[]): boolean {
         parent = parent[segment];
     }
     return true;
+}
+
+// makep
+// makep ensures the path exists on the document, creates if needed.
+// Always run the check function before runnign this function
+export function makep(doc: any, path: Path): void {
+    let parent = doc;
+    for (let i = 1, l = path.length; i < l; ++i) {
+        const parentSegment = path[i - 1];
+        const currentSegment = path[i];
+        if (typeof currentSegment === 'string') {
+            if (!parent[parentSegment]) {
+                parent[parentSegment] = {};
+            }
+        } else if (typeof currentSegment === 'number') {
+            if (!parent[parentSegment]) {
+                parent[parentSegment] = [];
+            }
+        }
+        parent = parent[parentSegment];
+    }
+}
+
+// isObject
+// isObject checks whether the input is an object
+export function isObject(val: any) {
+    return typeof val === 'object' && val !== null && !Array.isArray(val);
+}
+
+// isArray
+// isArray checks whether the input is an array
+export function isArray(val: any) {
+    return Array.isArray(val);
 }
