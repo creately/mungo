dfc579b001ff29b203d8195ea0c697c8e0e319c2
Muhammed Thanish <mnmtanish@gmail.com>
2017-09-22 13:33:07 +0530

    Add comments and tests for types

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,8 @@
 // DataType
-// DataType ...
+// DataType is a custom data type enum which can be used
+// instead of default data types in javascript. It can be
+// to used specify required types, validate and to get zero
+// values. It can be used with getType and getZero functions.
 export enum DataType {
   any,
   array,
@@ -10,7 +13,8 @@ export enum DataType {
 }
 
 // getType
-// getType ...
+// getType identifies the data type of given value. Returns null
+// if it was unable to identify the type. Never returns 'any' type.
 export function getType(val: any) {
   if (Array.isArray(val)) {
     return DataType.array;
@@ -27,11 +31,12 @@ export function getType(val: any) {
   if (typeof val === 'boolean') {
     return DataType.boolean;
   }
-  return null;
+  return DataType.any;
 }
 
 // getZero
-// getZero ...
+// getZero returns the zero value of given type. Returns null
+// if the type is not valid or for the 'any' data type.
 export function getZero(type: DataType): any {
   if (type === DataType.array) {
     return [];
